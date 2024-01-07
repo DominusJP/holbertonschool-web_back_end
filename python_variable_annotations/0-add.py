f2f48e2d641318102d0aaebd591b9a863fc79145
Juan Ferraro <6775@holbertonstudents.com>
2024-01-07 02:40:27 -0300

    01

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Python - Variable Annotations
+
+"""
+
+
+def add(a: float, b: float) -> float:
+    
+    """Adds two floating-point numbers and returns the result."""
+    return a + b
