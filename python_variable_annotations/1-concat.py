a40808ee2b9e539797ae526909e2dfd8c5fc7905
Juan Ferraro <6775@holbertonstudents.com>
2024-01-07 03:00:04 -0300

    1 y 2

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -3,9 +3,8 @@
 Python - Variable Annotations
 
 """
-import math
 
 
-def floor(n: float) -> int:
-    """Returns the floor of the given float."""
-    return math.floor(n)
+def concat(str1: str, str2: str) -> str:
+    """Concatenates two strings and returns the result."""
+    return str1 + str2
---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Python - Variable Annotations
+
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """Returns the floor of the given float."""
+    return math.floor(n)
