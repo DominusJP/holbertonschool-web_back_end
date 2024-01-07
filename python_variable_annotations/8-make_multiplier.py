b02e68cec7b43a491ddfb63b1db9134b0f13b1cf
Juan Ferraro <135638564+DominusJP@users.noreply.github.com>
2024-01-07 04:11:20 -0300

    Update 8-make_multiplier.py

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -10,5 +10,4 @@ def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """multiplies a float by the given multiplier."""
     def multiplier_function(x: float) -> float:
         return x * multiplier
-    
     return multiplier_function
