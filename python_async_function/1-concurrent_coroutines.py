19cab14b9a9fd8c4be98d4a5eb8adde36b9871e3
Juan Ferraro <135638564+DominusJP@users.noreply.github.com>
2024-01-07 06:20:22 -0300

    Update 1-concurrent_coroutines.py

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -2,9 +2,9 @@
 """Python - Async"""
 import asyncio
 from typing import List
-
-
 wait_random = __import__('0-basic_async_syntax').wait_random
+
+
 async def wait_n(n: int, max_delay: int = 10) -> List[float]:
     """Asynchronous coroutine"""
     tasks = [wait_random(max_delay) for _ in range(n)]
