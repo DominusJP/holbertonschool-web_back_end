debe394c6cdf8e9002f1fa3c9ebd51fb301d2e1d
Juan Ferraro <135638564+DominusJP@users.noreply.github.com>
2024-01-07 04:01:00 -0300

    Update 5-sum_list.py

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -7,5 +7,5 @@ from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
-    """Returns the sum of the given list of floats."""
+    """Returns the sum of the given list"""
     return sum(input_list)
