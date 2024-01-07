d6217aa37bf3424e0610d2473b1de66ea08e15c0
Juan Ferraro <6775@holbertonstudents.com>
2024-01-07 03:06:49 -0300

    3 y 4

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Python - Variable Annotations
+
+"""
+
+
+def to_str(n: float) -> str:
+    """Returns the string representation of the given float."""
+    return str(n)
