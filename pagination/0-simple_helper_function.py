c3210bff9ee12aaf27fad419426717b8e11b4498
Juan Ferraro <6775@holbertonstudents.com>
2024-01-08 02:32:10 -0300

    0

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Pagination"""
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    ''' Simple helper function'''
+    return ((page_size * (page - 1)), page_size * page)
