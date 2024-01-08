e9b947f54699b4082697918aeba573a8f14cab2f
Juan Ferraro <6775@holbertonstudents.com>
2024-01-07 22:04:07 -0300

    1.1

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """python_async_comprehension"""
-import asyncio
 import random
+import asyncio
 
 
 async def async_generator():
+    """0-async_generator"""
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
