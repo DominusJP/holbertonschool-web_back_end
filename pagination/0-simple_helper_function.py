8033ffa57421aee04e0e43b7d2637e817c30f593
Juan Ferraro <6775@holbertonstudents.com>
2024-01-08 03:03:24 -0300

    s

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -6,4 +6,3 @@ from typing import Tuple
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     '''Simple helper function'''
     return ((page_size * (page - 1)), page_size * page)
-    
