10b07abf361c681e60e06f94c32e25b48059adc4
Juan Ferraro <135638564+DominusJP@users.noreply.github.com>
2024-02-04 16:35:31 -0300

    Update 1-get_list_student_ids.js

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,7 +1,7 @@
 export default function getListStudentIds(arr) {
-    let respuesta = [];
-    if (Array.isArray(arr)) {
-      respuesta = arr.map((a) => a.id);
-    }
-    return respuesta;
+  let respuesta = [];
+  if (Array.isArray(arr)) {
+    respuesta = arr.map((a) => a.id);
   }
+  return respuesta;
+}
