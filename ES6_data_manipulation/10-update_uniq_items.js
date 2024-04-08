239dbb8088cf04e2b2d8b2af239a518bb13bb17e
Juan Ferraro <135638564+DominusJP@users.noreply.github.com>
2024-04-08 12:43:47 -0300

    Delete ES6_data_manipulation directory

--- ES6_data_manipulation/10-update_uniq_items.js
+++
@@ -1,7 +0,0 @@
-export default function updateUniqueItems(groceries) {
-  if (!(groceries instanceof Map)) throw TypeError('Cannot process');
-  for (const [item, count] of groceries) {
-    if (count === 1) groceries.set(item, 100);
-  }
-  return groceries;
-}
--- ES6_data_manipulation/2-get_students_by_loc.js
+++
@@ -1,4 +0,0 @@
-export default function getStudentsByLocation(arr, city) {
-  const result = arr.filter((arr) => arr.location === city);
-  return result;
-}
