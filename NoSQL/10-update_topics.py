bf2a3e80505e9e8573ec6b74676a18d866616d30
Juan Ferraro <135638564+DominusJP@users.noreply.github.com>
2024-04-08 12:41:28 -0300

    Delete NoSQL directory

--- NoSQL/10-update_topics.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""Change school topics"""
-
-
-def update_topics(mongo_collection, name, topics):
-    """changes all school topics"""
-    result = mongo_collection.update_many({'name': name},
-                                          {'$set': {'topics': topics}})
-    return result.modified_count
--- NoSQL/9-insert_school.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-"""Insert a document in Python"""
-
-
-def insert_school(mongo_collection, **kwargs):
-    """Insert a document in Python"""
-    document_id = mongo_collection.insert(kwargs)
-    return document_id
