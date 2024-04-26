14d55e66f96ad908b5d671a02e158f1f318b2e4f
Juan Ferraro <135638564+DominusJP@users.noreply.github.com>
2024-04-26 11:15:48 -0300

    Update 6-final-user.js

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,20 +2,13 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results) => {
-      const status = results.map((result) => {
-        if (result.status === 'rejected') {
-          return {
-            status: result.status,
-            value: result.reason.toString(),
-          };
-        }
-        return {
-          status: result.status,
-          value: result.value,
-        };
-      });
-      return status;
-    });
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((result) => (
+    result.map((data) => ({
+      status: data.status,
+      value: data.value ? data.value : String(data.reason),
+    }))
+  ));
 }
